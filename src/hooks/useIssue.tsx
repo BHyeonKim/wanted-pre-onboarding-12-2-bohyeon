162875836881bf43a2dbd6ec3c284709aa36606b
BHyeonKim <rlaqhguse@gmail.com>
2023-08-30 16:41:42 +0900

    feat: issue 데이터 패칭 훅 작성

---
+++ src/hooks/useIssue.tsx
@@ -0,0 +1,30 @@
+import { useCallback, useEffect, useState } from 'react'
+import { Issue } from 'types/types'
+import octokit from 'utils/octokit'
+
+const useIssue = (issueNumber: number) => {
+  const [issue, setIssue] = useState<Issue>()
+
+  const fetchIssue = useCallback(async () => {
+    const { data } = await octokit.request(
+      'GET /repos/{owner}/{repo}/issues/{issue_number}',
+      {
+        owner: 'facebook',
+        repo: 'react',
+        issue_number: issueNumber,
+        headers: {
+          'X-GitHub-Api-Version': '2022-11-28',
+        },
+      },
+    )
+    setIssue(data)
+  }, [issueNumber])
+
+  useEffect(() => {
+    fetchIssue()
+  }, [fetchIssue])
+
+  return issue
+}
+
+export default useIssue
