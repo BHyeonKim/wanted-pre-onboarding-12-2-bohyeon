84828a0d4a99645f90185b88cd575195ea6c758d
BHyeonKim <rlaqhguse@gmail.com>
2023-08-30 16:45:47 +0900

    feat: 라우터 정의

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 import 'styles/index.scss'
 
+import Layout from 'components/Layout'
+import DetailPage from 'pages/DetailPage'
 import IssueListPage from 'pages/IssueListPage'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
 
-const App = () => <IssueListPage />
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<Layout title="React" />} path="/">
+      <Route element={<IssueListPage />} path="" />
+      <Route element={<DetailPage />} path="detail/:issueNumber" />
+    </Route>,
+  ),
+)
+
+const App = () => <RouterProvider router={router} />
 
 export default App
